3f0c768b6dd13c7977717d23046bc005ff86ea4c
Andrey <AndrEkat96@yandex.ru>
2024-02-27 09:07:02 +0500

    Home#2 add input-validation

--- src/midlewares/input-validation-middleware.ts
+++ src/midlewares/input-validation-middleware.ts
@@ -15,24 +15,40 @@ const validateObjectIdMiddleware = (req: Request, res: Response, next: NextFunct
 
 export default validateObjectIdMiddleware;
 
-
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        const errResult = errors.array().map((error: ValidationError & {
-            param?: string,
-            location?: string
-        }) => ({
-            message: error.msg,
-            field: error.param || error.location || "unknown"
+        const errResult = errors.mapped();
+
+        const formattedErrors = Object.keys(errResult).map((key) => ({
+            message: errResult[key].msg,
+            field: key,
         }));
-        res.status(400).json({errorsMessages: errResult});
-        // res.status(400).json({codeResult: 1, errorsMessages: errResult});
+
+        res.status(400).json({ errorsMessages: formattedErrors });
     } else {
         next();
     }
 };
+
+// export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     const errors = validationResult(req);
+//
+//     if (!errors.isEmpty()) {
+//         const errResult = errors.array().map((error: ValidationError & {
+//             param?: string,
+//             location?: string
+//         }) => ({
+//             message: error.msg,
+//             field: error.param || error.location || "unknown"
+//         }));
+//         res.status(400).json({errorsMessages: errResult});
+//         // res.status(400).json({codeResult: 1, errorsMessages: errResult});
+//     } else {
+//         next();
+//     }
+// };
 
 // export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 //     const errors = validationResult(req);
